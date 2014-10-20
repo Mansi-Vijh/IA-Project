2bd18585212f4d1b16e804dde5507e9b11c2419f
Mansi Vijh <mansivijh94@gmail.com>
2014-10-20 22:17:03 +0530

    project created

---
+++ PicturePerfect/PicturePerfect/Main.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace PicturePerfect
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Original = new Emgu.CV.UI.ImageBox();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.browse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Original)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Original
+            // 
+            this.Original.Location = new System.Drawing.Point(121, 126);
+            this.Original.Name = "Original";
+            this.Original.Size = new System.Drawing.Size(518, 547);
+            this.Original.TabIndex = 2;
+            this.Original.TabStop = false;
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "openFileDialog1";
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(121, 46);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(139, 28);
+            this.browse.TabIndex = 3;
+            this.browse.Text = "Choose Image";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 750);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.Original);
+            this.Name = "Main";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Original)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox Original;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private System.Windows.Forms.Button browse;
+    }
+}
+
---
+++ PicturePerfect/PicturePerfect/Main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+namespace PicturePerfect
+{
+    public partial class Main : Form
+    {
+        string Chosen_File = "";
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            openFD.Title = "Insert an Image";
+            openFD.FileName = "";
+          //  openFD.Filter = " TIFF Images|*.tif|JPEG Images|*.jpg|GIF Images|*.gif|BITMAPS|*.bmp|PNG Images|*.png|PICT Images|*.pct";
+            if (openFD.ShowDialog() == DialogResult.Cancel)
+            {
+                MessageBox.Show("Choose image to proceed");
+            }
+            else
+            {
+                Chosen_File = openFD.FileName;
+                Image<Gray, byte> img = new Image<Gray, byte>(Chosen_File);
+                Original.Image = img;
+            }
+        }
+    }
+}
